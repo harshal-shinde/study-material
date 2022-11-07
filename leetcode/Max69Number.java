0ea8b3d9999f26bb0145a93b501ae11efaa5e960
h2s <harshaal.shinde@gmail.com>
2022-11-07 18:05:37 +0530

    Maximum 69 number

---
+++ leetcode/Max69Number.java
@@ -0,0 +1,19 @@
+
+/**
+ * Problem number - 1323 
+ * You are given a positive integer num consisting only of digits 6 and 9.
+    Return the maximum number you can get by changing at most one digit
+    (6 becomes 9, and 9 becomes 6).
+ */
+class Max69 {
+    public int maximum69Number (int num) {
+       char[] charArray = String.valueOf(num).toCharArray();
+        for(int i=0; i< charArray.length; i++) {
+            if(charArray[i] == '6') {
+                charArray[i] = '9';
+                break;
+            }
+        }
+        return Integer.valueOf(String.valueOf(charArray));
+    }
+}
