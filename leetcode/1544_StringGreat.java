d6dec0e3e267af333b2f18c741826c165a243d02
h2s <harshaal.shinde@gmail.com>
2022-11-08 15:04:34 +0530

    Problem 1544 - Make string great

---
+++ leetcode/1544_StringGreat.java
@@ -0,0 +1,19 @@
+/*
+ * https://leetcode.com/problems/make-the-string-great/
+ */
+class Solution {
+    public String makeGood(String s) {
+        var sb = new StringBuilder();
+        for(int i=0; i< s.length(); ++i) {
+            int l =sb.length();
+            if(l>0 && Math.abs(sb.charAt(l-1) - s.charAt(i)) == 32) {
+                sb.setLength(l-1);
+            }else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
